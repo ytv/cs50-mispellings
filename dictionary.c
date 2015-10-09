a59ef57d6a4ce3ae33e5c9c75fb367b0ba24c64f
ytv <sillying@gmail.com>
2015-10-09 13:05:20 -0400

    uses a trie

--- dictionary.c
+++ dictionary.c
@@ -1,5 +1,8 @@
-// uses hash function
-// prints misspelled words exactly how they appear in the text
+/* ***********
+ * by ytv
+ * CS50x
+ * uses a trie
+ /
 
 /****************************************************************************
  * dictionary.c
@@ -19,61 +22,60 @@
 
 typedef struct node
 {
-    char word[LENGTH + 1];
-    struct node* next;
+    struct node* letter[27];
+    bool end_of_word;
 } node;
 
-// create a hash table
-struct node* hash[26] = {NULL};
+// create the root table
+struct node* root;
 
 int word_count = 0;
+
+bool free_funct(struct node* node);
 
 /**
  * Returns true if word is in dictionary else false.
  */
 bool check(const char* word)
 {
-    int j = tolower(word[0])-'a';
+    bool found = true;
+    int z = strlen(word), i = 0;
+    node* cursor = root;
     
-    bool found = false, eo_list = false;
-    
-    node* cursor = hash[j];
-    
-    if(cursor == NULL)
+    char temp_2[LENGTH+1];
+    strcpy(temp_2, word);
+        
+    for(int i = 0; i < z; i++)
     {
-        return false;
+        temp_2[i] = tolower(temp_2[i]);
     }
-    else
-    {
-        // make the word completely undercase
+    
+    
+    do{
+        int index = temp_2[i] - 'a';
         
-        int z = strlen(word);
-        char low_case_word[z];
-        strcpy(low_case_word, word);
-        
-        for(int i = 0; i < z; i++)
+        if(cursor->letter[index] == NULL)
         {
-            low_case_word[i] = tolower(low_case_word[i]);
+            found = false;
+        }
+        else
+        {
+            cursor = cursor->letter[index];
+
+            if(i == z - 1)
+            {
+                if(cursor->end_of_word == false)
+                {
+                    found = false;
+                }
+            }
+            
+            i++;
         }
         
-        //sanity check
-        //printf("after using tolower, the word is %s\n", word);
-        
-        do
-        {
-            if(strcmp(cursor->word, low_case_word) == 0)
-                found = true;
-            else
-            {
-                if(cursor->next != NULL)
-                    cursor = cursor->next;
-                else
-                    eo_list = true;
-            }
-        } while((found == false) && (eo_list == false));
-    
-        return found;
-    }
+    }while(i < z && found == true);
+  
+    return found;
 }
 
 /**
@@ -81,67 +83,58 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // open up the dictionary file
+    root = calloc(1, sizeof(node));
+    
+    // create new_node and load in word
     FILE* fp;
     fp = fopen(dictionary, "r");
-    
+       
     if (!fp)
     {
         printf("Error: unable to open dictionary file\n");
-        return false; //update  
+        return false; 
     }
     else
     {
+        struct node* cursor;
         char temp_1[LENGTH+1];
         
         while(fscanf(fp, "%s", temp_1) != EOF)
-        {
+        {            
             word_count++;
-           // printf("%s\n", temp_1);
             
-            // create new node and load in the new word from dictionary into the new_node    
-            node* new_node = malloc(sizeof(node));
-            new_node->next = NULL;
-            strcpy(new_node->word, temp_1);
+            int len = strlen(temp_1);
+
+            cursor = root;
             
-            char c = new_node->word[0];
-            int k = c - 'a';
-           // printf("%i\n", k);
-            
-            // check if hash[k] is already pointing to a node or not
-            // if no, assign hash[k] to point to new node
-            // if yes, traverse the hash[k] linked list until it finds its end and append to the end
-            if(hash[k] == NULL)
+            for(int index = 0; index <= len; index++)
             {
-                
-                hash[k] = new_node;
-            }
-            else
-            {
-                node* temp_2 = hash[k];
-                
-                while(temp_2->next != NULL)
+                if(index == len)
                 {
-                    temp_2 = temp_2->next;
+                    cursor->end_of_word = true;
                 }
-                temp_2->next = new_node;
+                else
+                {
+                    int k = temp_1[index] - 'a';
+                    
+                    //if letter[k] is pointing to something, go to it
+                    //otherwise, create a new node and then point to it
+                    if(cursor->letter[k] != NULL)
+                    {
+                        cursor = cursor->letter[k];
+                    }
+                    else
+                    {
+                        node* new_node = calloc(1, sizeof(node));
+                        new_node->end_of_word = false;
+                        
+                        cursor->letter[k] = new_node;
+                        cursor = cursor->letter[k];
+                    }
+                }
             }
         }
     }
-   
-    // sanity check to make sure all words were loaded
-    /*for(int i = 0; i < 26; i++)
-    {
-        printf("hash[%c]: ", i + 'a');
-        
-        node* node_to_print = hash[i];
-        while(node_to_print != NULL){
-            printf("%s -> ", node_to_print->word);
-            node_to_print = node_to_print->next;
-        }
-        printf("\n");
-    }*/
-
     fclose(fp);
     return true;
 }
@@ -159,18 +152,23 @@ unsigned int size(void)
  */
 bool unload(void)
 {
-    for(int i = 0; i < 26; i++)
+    return free_funct(root);
+}
+
+bool free_funct(struct node* node)
+{
+    int i = 0;
+    
+    while(i < 27)
     {
-        //free_func(hash[i]);
-        struct node* cursor_2 = hash[i];
-        struct node* to_free;
-        
-        while((to_free = cursor_2) != NULL)
+        if(node->letter[i] != NULL)
         {
-            cursor_2 = cursor_2->next;
-            free(to_free);
+            free_funct(node->letter[i]); 
         }
+        i++;
     }
+    
+    free(node);
     
     return true;
 }
